c65eb22daddd030e96565881fc4e3fb075cb3b28
Gabriele Masina <gabriele.masina@virgilio.it>
2022-06-05 23:33:55 +0200

    nvchad custom: remove command line arguments for python formatters/linters, line-length configurable in pyproject.toml (black, isort) and setup.cfg (flake8)

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ M.setup_lsp = function(attach, capabilities)
   local lspconfig = require "lspconfig"
 
   -- lspservers with default config
-  local servers = { "sumneko_lua", "ccls", "html", "pyright" }
+  local servers = { "sumneko_lua", "ccls", "html", "pyright", "rust_analyzer" }
 
   for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
@@ -17,7 +17,7 @@ M.setup_lsp = function(attach, capabilities)
     settings = {
       Lua = {
         diagnostics = {
-          globals = { 'vim', 'use'}
+          globals = { 'vim', 'use' }
         }
       }
     }
